9787661624a17cde6a4b367533caa45d8b7cbd91
PaperFox56 <paperfox56@gmail.com>
2025-09-26 17:35:05 +0000

    Update tests.py
    comments

--- tests.py
+++ tests.py
@@ -1,14 +1,17 @@
 from saves.circlesGenerator import *
 from NoiseRemover import *
 
+## Load the last saved model
 network = SimpleForward(layers, size)
 network.weights = weights
 network.bias = bias
 
+# generate an output from a random image
 a = np.random.random(size)
 result = network.predicte(a)[-1]
 s = 10
 result.resize(s, s)
 a.resize(s, s)
+
 #view(a, 30)
 view(result, 30)
